f3506f704412c8481052b5e8be8b16bfd9a33611
sushantdhakal25 <shakti.punk25@hotmail.com>
2019-02-22 19:48:55 +0545

    Hello Toast Challenge Completed

---
+++ Week1/HelloToastChallenge/app/src/main/java/com/example/hellotoastchallenge/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hellotoastchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ private int ncount;
+ private TextView textcount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textcount=(TextView) findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        ncount++;
+        textcount.setText(""+ncount);
+    }
+
+    public void showtoast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
