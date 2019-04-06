71583149fb896b01963d6de6967a44578d7bfa4e
sushantdhakal25 <shakti.punk25@hotmail.com>
2019-04-06 19:25:09 +0545

    01.1: Fragments- Task 1 Completed

---
+++ Week6/FragmentExample1/app/src/main/java/com/example/android/fragmentexample1/RatingFragment.java
@@ -0,0 +1,45 @@
+package com.example.android.fragmentexample1;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+
+public class RatingFragment extends Fragment {
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public RatingFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView =
+                inflater.inflate(R.layout.fragment_rating, container, false);
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                int value = (int) rating;
+                showToastMethod(value  + "");
+            }
+        });
+
+        return rootView;
+    }
+
+    public void showToastMethod(String msg) {
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
